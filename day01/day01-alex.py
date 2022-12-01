d2b628beff3debacdcfaca37b90823db35e68d2c
AlexanderHeinzYagora <alexander.heinz@yagora.com>
2022-12-01 17:57:03 +0100

    first commit

---
+++ day01/day01-alex.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import heapq
+
+day01 = pd.read_excel("day01.xlsx")
+cs = day01.apply(lambda x: x.groupby((np.isnan(x)).cumsum()).cumsum())
+
+np.max(cs) # solution part 1 
+
+sum(heapq.nlargest(3, cs["numbers"])) # solution part 2
